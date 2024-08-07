49acd8a563d77471738866d93e9665d9042c2b2f
SamratMali <samratmali6569@gmail.com>
2024-08-07 23:35:11 +0530

    update script

--- script.js
+++ script.js
@@ -3,18 +3,17 @@ const lengthDisplay = document.querySelector("[data-lengthNumber]");
 const passwordDisplay = document.querySelector("[data-passwordDisplay]");
 const copyBtn = document.querySelector("[data-copy]");
 const copyMsg = document.querySelector("[data-copyMsg]");
-const uppercaseCheck = document.querySelector("[#uppercase]");
-const lowercaseCheck = document.querySelector("[#lowercase]");
-const numbersCheck = document.querySelector("[#numbers]");
-const symbolsCheck = document.querySelector("[#symbols]");
+const uppercaseCheck = document.querySelector("#uppercase");
+const lowercaseCheck = document.querySelector("#lowercase");
+const numbersCheck = document.querySelector("#number");
+const symbolsCheck = document.querySelector("#symbols");
 const indicator = document.querySelector("[data-indicator]");
-const generateBtn = document.querySelector("[.generateBtn]");
-const allCheckBox = document.getElementById("input[type=checkbox]");
+const generateBtn = document.querySelector(".generateButton");
+const allCheckBox = document.querySelectorAll("input[type=checkbox]");
 const symbols = '~`!@#$%^&*()_+={}[]:;"<,>.?/';
 
 let password = "";
 let passwordLength = 15;
-let checkCount = 1;
 
 handleSlider();
 
@@ -43,3 +42,45 @@ function generateSymbol() {
   const randNum = getRndInteger(0, symbols.length);
   return symbols.charAt(randNum);
 }
+
+function calcStrength() {
+  let hasUpper = false;
+  let hasLower = false;
+  let hasNum = false;
+  let hasSym = false;
+
+  if (uppercaseCheck.checked) hasUpper = true;
+  if (lowercaseCheck.checked) hasLower = true;
+  if (numbersCheck.checked) hasNum = true;
+  if (symbolsCheck.checked) hasSym = true;
+
+  if (hasUpper && hasLower && (hasNum || hasSym) && passwordLength >= 8) {
+    setIndicator("#0f0");
+  } else if (
+    (hasLower || hasUpper) &&
+    (hasNum || hasSym) &&
+    passwordLength >= 6
+  ) {
+    setIndicator("#ff0");
+  } else {
+    setIndicator("#f00");
+  }
+}
+async function copyContent() {
+  try {
+    await navigator.clipboard.writeText(passwordDisplay.value);
+    copyMsg.innerText = "copied";
+  } catch (e) {
+    copyMsg.innerText = "Failed";
+  }
+  //To Make Copy Wala Span Visible
+  copyMsg.classList.add("active");
+
+  setTimeout(() => {
+    copyMsg.classList.remove("active");
+  }, 2000);
+}
+inputSlider.addEventListener("input", (e) => {
+  passwordLength = e.target.value;
+  handleSlider();
+});
